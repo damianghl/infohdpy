efd1e043ea3d4bb0df01ff8b842e834bd20796f4
d3mian <damian.hernandez@ib.edu.ar>
2024-07-30 16:54:36 -0300

    fixing error in logLbT, extra +1, 2

--- info_hdp.py
+++ info_hdp.py
@@ -702,7 +702,7 @@ class InfoHDP:
         """
         kx, Ny = nxy.shape
         ll = kx * (special.gammaln(b) - np.sum(special.gammaln(b * qy)))
-        ll += np.sum(np.sum(special.gammaln(1 + b * qy + nxy), axis=1) - special.gammaln(b + np.sum(nxy, axis=1)))
+        ll += np.sum(np.sum(special.gammaln(b * qy + nxy), axis=1) - special.gammaln(b + np.sum(nxy, axis=1)))
         return ll
     
     @classmethod
