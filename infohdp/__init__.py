4d80733f430b640c268b2d35e466610ca70b2672
d3mian <damian.hernandez@ib.edu.ar>
2024-07-31 15:58:59 -0300

    fixing init, adding new functions

--- infohdp/__init__.py
+++ infohdp/__init__.py
@@ -1,26 +1,24 @@
 # Import core functionalities
-from .core import *
+from .core import strue, sxtrue, sytrue, itrue
 
 # Import estimators
-#from .estimators import naive, nsb, infohdp, binary_infohdp, multiclass_infohdp
-from .estimators import naive, nsb
+from .estimators import naive, nsb, binary_infohdp, multiclass_infohdp
 
 # Import generators
 from .generators import probability, sample
 
 # Import utilities
-from .utils import *
+from .utils import dkm2, n10sam, nxysam
 
 # Define version
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Define all exported symbols
 __all__ = [
     "naive",
     "nsb",
-#    "infohdp",
-#    "binary_infohdp",
-#    "multiclass_infohdp",
+    "binary_infohdp",
+    "multiclass_infohdp",
     "probability",
     "sample",
 ]
